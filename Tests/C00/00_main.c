a6000ead3d7aa547986b8ae185b2b5c6278bdfe1
Korkrane <48218250+Korkrane@users.noreply.github.com>
2020-02-19 17:15:34 +0100

    [WIP] Tests_C00_ex00

--- Tests/C00/00_main.c
+++ Tests/C00/00_main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 #define ANSI_COLOR_RESET   "\x1b[0m"
 #define ANSI_COLOR_RED     "\x1b[31m"
@@ -8,22 +9,20 @@ void ft_putchar(char c);
 
 int main()
 {
-	int n;
-	int i;
-	n = 1;
-	i = 0;
+	int n = 1;
+	int i = 0;
 	char *value_test = "c0#²";
 
 	while(n < 5)
 	{
+		int expected = putchar(value_test[i]);
 		printf("****** TEST N°%d ******\n", n);
-    	ft_putchar(value_test[i]);
-    	printf("\n");
-		printf("Expected result : %c\n", value_test[i]);
-		//if(your == expected)
-			//printf(ANSI_COLOR_GREEN   "      [CORRECT]\n"   ANSI_COLOR_RESET "\n");
-		//else
-		//	printf(ANSI_COLOR_RED	"      [CORRECT]\n"   ANSI_COLOR_RESET "\n");
+    	printf("Your result : %p\n", ft_putchar(value_test[i]));
+		printf("Expected result : %c\n", expected);
+		if(ft_putchar(value_test[i]) == expected)
+			printf(ANSI_COLOR_GREEN   "      [CORRECT]\n"   ANSI_COLOR_RESET "\n");
+		else
+			printf(ANSI_COLOR_RED	"      [CORRECT]\n"   ANSI_COLOR_RESET "\n");
 		i++;
 		n++;
 	}
